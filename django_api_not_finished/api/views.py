c682fb3857e886ed25935dfeabba6653bc0e7015
skullofproxy7 <81231137+skullofproxy7@users.noreply.github.com>
2021-03-25 12:35:43 +0200

    Update views.py

--- django_api_not_finished/api/views.py
+++ django_api_not_finished/api/views.py
@@ -1,5 +1,9 @@
+from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from api.serializers import *
 from api.models import *
 
 @api_view(["GET"])
@@ -10,10 +14,10 @@ def Registrayion(request):
 @api_view(["POST"])
 def Registrayion(request):
     track_uniq_id=request.META['HTTP_X_CORELATIONID']
-    f = ContactForm(request.POST)
+    f = ContactForm(request.data)
     if f.is_valid():
         sf = f.save()
-        content = {"registeredid": "uuid"}#uuid
+        content = {"registeredid": sf.id}
         return JsonResponse(content)
     else:
         content = {"message": "error"}
