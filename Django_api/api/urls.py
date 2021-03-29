a4ecd07118b1010a5f530d62d39831eecc789e4a
skullofproxy7 <81231137+skullofproxy7@users.noreply.github.com>
2021-03-29 15:32:22 +0300

    Update urls.py

--- Django_api/api/urls.py
+++ Django_api/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('v1/registration/<uuid:registrationId>', views.RegistrayionRequest),
-    path('v1/registration/', views.Registrayion),
+    path('v1/registration/<uuid:registrationId>', views.RegistrationRequest),
+    path('v1/registration', views.Registration),
 ]
