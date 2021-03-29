65218e301b395778673a50f5d9e554877fdccda4
skullofproxy7 <81231137+skullofproxy7@users.noreply.github.com>
2021-03-29 13:59:09 +0300

    Delete serializers.py

--- django_api_not_finished/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from api.models import *
-
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contact
-        fields = ['locale']
