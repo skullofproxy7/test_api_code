428a19a7c19bf820dd678085538f785adca5723f
skullofproxy7 <81231137+skullofproxy7@users.noreply.github.com>
2021-03-29 14:08:00 +0300

    Delete django_api_not_finished directory

--- django_api_not_finished/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path('v1/registration/<uuid:registrationId>', views.RegistrayionRequest),
-    path('v1/registration/', views.Registrayion),
-]
